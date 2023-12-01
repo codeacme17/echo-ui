2a1ce98f08bfa933d946c3ed61f9f9c47a7aee63
leyoonafr <vir_kings@qq.com>
2023-12-01 16:43:15 +0800

    chore: config path and upload example dir

---
+++ example/vite.config.ts
@@ -0,0 +1,13 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '../src'),
+    },
+  },
+})
---
+++ src/copmponent/control/Knobs.tsx
@@ -0,0 +1,3 @@
+export const Knobs = () => {
+  return <section>Knobs</section>
+}
