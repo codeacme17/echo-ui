014d2c1bb986aa9c3006dbd99657344035c180b4
leyoonafr <vir_kings@qq.com>
2023-12-06 10:00:00 +0800

    chore(website): add tailwindcss

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -28,6 +28,18 @@ const config: Config = {
         },
       },
     ],
+
+    async function myPlugin(context, options) {
+      return {
+        name: 'docusaurus-tailwindcss',
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require('tailwindcss'))
+          postcssOptions.plugins.push(require('autoprefixer'))
+          return postcssOptions
+        },
+      }
+    },
   ],
 
   presets: [
