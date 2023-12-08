3d4908a98d689718089d6aab77e996d92f2e333c
codeacme17 <vir_kings@qq.com>
2023-12-08 18:37:01 +0800

    opt(Slider): optimize performence

--- packages/components/control/Slider/index.tsx
+++ packages/components/control/Slider/index.tsx
@@ -1,97 +1,108 @@
-import { useEffect, useRef, useState } from 'react'
+import { memo, useEffect, useRef, useState, useCallback } from 'react'
 import { SliderProps } from './types'
 import { cn } from '../../../lib/utils'
 import './styles.css'
 
-export const Slider = ({
-  min = 0,
-  max = 100,
-  step = 1,
-  defaultValue = 20,
-  vertical = false,
-  showThumb = true,
-  interactive = true,
-  value: dynamicValue,
-  onChange,
-  ...props
-}: SliderProps) => {
-  const [value, setValue] = useState(defaultValue)
-  const [isDragging, setIsDragging] = useState(false)
-  const sliderRef = useRef(null)
+export const Slider = memo(
+  ({
+    min = 0,
+    max = 100,
+    step = 1,
+    defaultValue = 20,
+    vertical = false,
+    showThumb = true,
+    interactive = true,
+    value: dynamicValue,
+    onChange,
+    ...props
+  }: SliderProps) => {
+    const [value, setValue] = useState(defaultValue)
+    const [isDragging, setIsDragging] = useState(false)
+    const sliderRef = useRef(null)
+    const sliderRect = useRef({ left: 0, width: 0, bottom: 0, height: 0 })
 
-  const startDragging = (e: React.MouseEvent) => {
-    if (!interactive) return
-    setIsDragging(true)
-    updateValue(e)
-  }
+    const updateValue = useCallback(
+      (e: MouseEvent | React.MouseEvent) => {
+        e.stopPropagation()
 
-  const onDrag = (e: MouseEvent) => {
-    if (!isDragging) return
-    e.preventDefault()
-    updateValue(e)
-  }
+        const { left, width, bottom, height } = sliderRect.current
+        let ratio: number
+        if (vertical) ratio = (bottom - e.clientY) / height
+        else ratio = (e.clientX - left) / width
 
-  const stopDragging = (e: MouseEvent) => {
-    e.preventDefault()
-    setIsDragging(false)
-  }
+        let newValue = ratio * (max - min) + min
+        newValue = parseFloat((Math.round(newValue / step) * step).toFixed(10))
+        newValue = Math.max(min, Math.min(newValue, max))
 
-  const updateValue = (e: MouseEvent | React.MouseEvent) => {
-    e.stopPropagation()
+        setValue(newValue)
+        if (onChange) onChange(newValue)
+      },
+      [min, max, step, vertical, onChange],
+    )
 
-    if (!sliderRef.current) return
-    const slider = sliderRef.current as HTMLDivElement
-    const { left, width, bottom, height } = slider.getBoundingClientRect()
+    const startDragging = useCallback(
+      (e: React.MouseEvent) => {
+        if (!interactive) return
+        setIsDragging(true)
+        if (sliderRef.current) {
+          const slider = sliderRef.current as HTMLDivElement
+          sliderRect.current = slider.getBoundingClientRect()
+        }
+        updateValue(e)
+      },
+      [interactive, updateValue],
+    )
 
-    let ratio: number
-    if (vertical) ratio = (bottom - e.clientY) / height
-    else ratio = (e.clientX - left) / width
+    const onDrag = useCallback(
+      (e: MouseEvent) => {
+        if (!isDragging) return
+        e.preventDefault()
+        requestAnimationFrame(() => updateValue(e))
+      },
+      [isDragging, updateValue],
+    )
 
-    let newValue = ratio * (max - min) + min
-    newValue = parseFloat((Math.round(newValue / step) * step).toFixed(10))
-    newValue = Math.max(min, Math.min(newValue, max))
+    const stopDragging = useCallback((e: MouseEvent) => {
+      e.preventDefault()
+      setIsDragging(false)
+    }, [])
 
-    setValue(newValue)
-    if (onChange) onChange(newValue)
-  }
+    useEffect(() => {
+      if (dynamicValue !== undefined) setValue(dynamicValue)
+    }, [dynamicValue])
 
-  useEffect(() => {
-    if (dynamicValue === undefined) return
-    setValue(dynamicValue)
-  }, [dynamicValue])
+    useEffect(() => {
+      document.addEventListener('mousemove', onDrag)
+      document.addEventListener('mouseup', stopDragging)
 
-  useEffect(() => {
-    document.addEventListener('mousemove', onDrag)
-    document.addEventListener('mouseup', stopDragging)
-
-    return () => {
-      document.removeEventListener('mousemove', onDrag)
-      document.removeEventListener('mouseup', stopDragging)
-    }
-  }, [isDragging])
-
-  return (
-    <div
-      className={cn(
-        'echo-slider',
-        vertical && 'echo-slider-vertical',
-        interactive && 'cursor-pointer',
-        props.className,
-      )}
-      ref={sliderRef}
-      onMouseDown={startDragging}
-    >
+      return () => {
+        document.removeEventListener('mousemove', onDrag)
+        document.removeEventListener('mouseup', stopDragging)
+      }
+    }, [onDrag, stopDragging])
+    return (
       <div
-        className={cn('echo-slider-track', vertical && 'echo-slider-track-vertical')}
-        style={{ [vertical ? 'height' : 'width']: `${((value - min) / (max - min)) * 100}%` }}
-      />
-
-      {showThumb && (
+        className={cn(
+          'echo-slider',
+          vertical && 'echo-slider-vertical',
+          interactive && 'cursor-pointer',
+          props.className,
+        )}
+        ref={sliderRef}
+        onMouseDown={startDragging}
+      >
         <div
-          className={cn('echo-slider-thumb', vertical && 'echo-slider-thumb-vertical')}
-          style={{ [vertical ? 'bottom' : 'left']: `${((value - min) / (max - min)) * 100}%` }}
+          className={cn('echo-slider-track', vertical && 'echo-slider-track-vertical')}
+          style={{ [vertical ? 'height' : 'width']: `${((value - min) / (max - min)) * 100}%` }}
         />
-      )}
-    </div>
-  )
-}
+
+        {showThumb && (
+          <div
+            className={cn('echo-slider-thumb', vertical && 'echo-slider-thumb-vertical')}
+            style={{ [vertical ? 'bottom' : 'left']: `${((value - min) / (max - min)) * 100}%` }}
+          />
+        )}
+      </div>
+    )
+  },
+)
