e6d5c068b0c7f374b44918e352bea957c265ecb0
codeacme17 <vir_kings@qq.com>
2023-12-10 19:54:47 +0800

    feat: add brand log to project

--- packages/lib/log.ts
+++ packages/lib/log.ts
@@ -1,7 +1,14 @@
+import { version } from '../../package.json'
 const PREFIX = 'Echo UI: '
 
 export const logger = {
   info: (...args: string[]) => console.log(PREFIX, ...args),
   warn: (...args: string[]) => console.warn(PREFIX, ...args),
   error: (...args: string[]) => console.error(PREFIX, ...args),
+}
+
+export const logBrand = () => {
+  const content = `%c > Echo UI v${version} prod by leyoonafr < `
+
+  console.log(content, 'background: #ffbe3b; color: #000')
 }
--- packages/main.ts
+++ packages/main.ts
@@ -2,4 +2,7 @@ export * from './components/control'
 export * from './components/visualization'
 export * from './components/utility'
 
+import { logBrand } from './lib/log'
 import './global.css'
+
+logBrand()
