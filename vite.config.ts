87f161d4035a8a03649d486863579841d919b817
leyoonafr <vir_kings@qq.com>
2023-12-15 17:22:29 +0800

    refactor: remove vitest file and extract config to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  test: {
+    environment: 'jsdom',
+    exclude: ['example/*', 'node_modules/**/*'],
+  },
 })
--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    exclude: ['example/*', 'node_modules/**/*'],
-  },
-})
