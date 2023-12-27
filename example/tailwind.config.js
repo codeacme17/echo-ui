7f2acac538fbf53a8ecd72f814e5786300be6200
leyoonafr <vir_kings@qq.com>
2023-12-27 15:51:21 +0800

    chore: restructure dirs

--- example/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-import { theme } from '../tailwind-theme.ts'
-
-export default {
-  darkMode: ['class'],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', '../packages/**/*.{js,ts,jsx,tsx}'],
-  theme,
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ import { theme } from './tailwind-theme'
 
 export default {
   darkMode: ['class'],
-  content: ['./packages/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './example/index.html',
+    './example/src/**/*.{js,ts,jsx,tsx}',
+    './packages/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: theme,
 }
