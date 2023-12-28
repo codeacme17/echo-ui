9896a13537a03aa51a93cde8cb4472afe46bb7f1
leyoonafr <vir_kings@qq.com>
2023-12-28 16:04:38 +0800

    docs(Button): add button component usages

---
+++ docs/src/components/Button.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { UsageBox } from './UsageBox'
+import { Button, SineIcon, SawtoothIcon, SquareIcon, TriangleIcon } from 'echo-ui'
+
+export const DefaultButton = () => {
+  const scope = { Button }
+  const code = `<Button> Button </Button>`
+
+  return <UsageBox code={code} scope={scope} />
+}
+
+export const ToggledButton = () => {
+  const scope = { Button }
+  const code = `<Button toggled> Toggled </Button>`
+
+  return <UsageBox code={code} scope={scope} />
+}
+
+export const DisabledButton = () => {
+  const scope = { Button }
+  const code = `<Button disabled> Disabled </Button>`
+
+  return <UsageBox code={code} scope={scope} />
+}
+
+export const SizeButton = () => {
+  const scope = { Button }
+  const code = `<div className="flex gap-4 items-center">
+  <Button size="sm">
+    Small
+  </Button>  
+  <Button size="md">
+    Medium
+  </Button>  
+  <Button size="lg">
+    Large
+  </Button>  
+</div>`
+
+  return <UsageBox code={code} scope={scope} />
+}
+
+export const RadiusButton = () => {
+  const scope = { Button }
+  const code = `<div className="flex gap-4 items-center">
+  <Button radius="none">
+    None
+  </Button>  
+  <Button radius="sm">
+    Small
+  </Button>  
+  <Button radius="md">
+    Medium
+  </Button>  
+  <Button radius="lg">
+    Large
+  </Button>
+  <Button radius="full">
+    Full
+  </Button>  
+</div>`
+
+  return <UsageBox code={code} scope={scope} />
+}
+
+export const GroupButton = () => {
+  const scope = { Button, SineIcon, SawtoothIcon, SquareIcon, TriangleIcon }
+
+  const code = `<Button.Group>
+  <Button value={1} className="data-[toggled=true]:bg-red-500">
+    Sine
+  </Button>
+  <Button value={2}>
+    Square
+  </Button>
+  <Button value={3}>
+    Sawtooth
+  </Button>
+  <Button value={4}>
+    Triangle
+  </Button>
+</Button.Group>`
+
+  return <UsageBox code={code} scope={scope} />
+}
--- docs/src/components/UsageBox.tsx
+++ docs/src/components/UsageBox.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Button } from 'echo-ui'
 import { themes } from 'prism-react-renderer'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 import { NextUIProvider } from '@nextui-org/react'
@@ -7,20 +6,21 @@ import { Tabs, Tab, Card, CardBody } from '@nextui-org/react'
 import { useDarkMode } from '../hooks/useDarkMode'
 import 'echo-ui/dist/style.css'
 
-export const UsageBox = () => {
+export const UsageBox = ({ code, scope }) => {
   const darkMode = useDarkMode()
-  const scope = { Button }
-  const code = `<Button>Button</Button>`
 
   return (
     <NextUIProvider>
       <LiveProvider
         code={code}
         scope={scope}
-        theme={darkMode ? themes.shadesOfPurple : themes.github}
+        theme={darkMode ? themes.shadesOfPurple : themes.oneLight}
       >
         <Tabs
           aria-label="Usage Box"
+          className="mt-3"
+          color="primary"
+          variant="underlined"
           items={[
             { id: 'preview', label: 'Preview' },
             { id: 'code', label: 'Code' },
@@ -28,21 +28,23 @@ export const UsageBox = () => {
         >
           {(item) => (
             <Tab key={item.id} title={item.label}>
-              <Card>
-                {item.id === 'preview' && (
-                  <CardBody className="w-full p-5 flex justify-center bg-background rounded-lg">
+              {item.id === 'preview' && (
+                <Card shadow="none" className="border border-border bg-transparent">
+                  <CardBody className="w-full p-5 flex justify-center  rounded-lg">
                     <LivePreview />
                   </CardBody>
-                )}
+                </Card>
+              )}
 
-                {item.id === 'code' && (
+              {item.id === 'code' && (
+                <Card shadow="none" className="bg-transparent">
                   <CardBody className="w-full p-5 flex justify-center bg-background rounded-lg">
                     <code className="rounded-lg overflow-hidden">
                       <LiveEditor className="bg-background" />
                     </code>
                   </CardBody>
-                )}
-              </Card>
+                </Card>
+              )}
             </Tab>
           )}
         </Tabs>
