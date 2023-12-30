afaa6d775e0c4d055d250e0e5ff3b0d017530be0
codeacme17 <vir_kings@qq.com>
2023-12-30 19:50:51 +0800

    refactor(Switch): refactor style file

--- packages/components/controller/Switch/Switch.tsx
+++ packages/components/controller/Switch/Switch.tsx
@@ -1,8 +1,8 @@
-import { forwardRef, useEffect, useMemo, useState } from 'react'
+import { forwardRef, useEffect, useState } from 'react'
+import { cn } from '../../../lib/utils'
 import { SwitchProps, SwitchRef } from './types'
 import { SIZE } from './constants'
-import { cn } from '../../../lib/utils'
-import STYLES from './styles.module.css'
+import { useSwitchStyle } from './styles'
 
 export const Switch = forwardRef<SwitchRef, SwitchProps>((props, ref) => {
   const {
@@ -16,8 +16,8 @@ export const Switch = forwardRef<SwitchRef, SwitchProps>((props, ref) => {
     ...restProps
   } = props
 
-  // =========== events ============= //
   const [toggled, setToggled] = useState(_toggled) // useState to track the toggled state
+
   // useEffect to call onChange prop whenever toggled state changes
   useEffect(() => {
     if (disabled) return
@@ -36,13 +36,7 @@ export const Switch = forwardRef<SwitchRef, SwitchProps>((props, ref) => {
     onClick && onClick(e)
   }
 
-  // =========== styles ============= //
-  // Define the class names based on the size prop
-  const sizeClassNames = useMemo(() => {
-    if (size === 'sm') return { button: 'w-12 h-6', thumb: 'w-3 h-3', label: 'text-sm' }
-    if (size === 'lg') return { button: 'w-16 h-10', thumb: 'w-5 h-5', label: 'text-lg' }
-    return { button: 'w-14 h-8', thumb: 'w-4 h-4' }
-  }, [size])
+  const { base, button, thumb, label } = useSwitchStyle({ disabled, toggled, size })
 
   return (
     <label
@@ -50,44 +44,21 @@ export const Switch = forwardRef<SwitchRef, SwitchProps>((props, ref) => {
       ref={ref}
       data-toggled={toggled}
       data-disabled={disabled}
-      className={cn(
-        'group',
-        STYLES['echo-switch'],
-        restProps.className,
-        disabled && STYLES['echo-switch__disabled'],
-      )}
+      className={cn(base(), restProps.className)}
       style={restProps.style}
       onClick={handleClick}
     >
       {/* Button */}
-      <span
-        className={cn(
-          STYLES['echo-switch-button'],
-          sizeClassNames.button,
-          classNames?.button,
-          toggled && STYLES['echo-switch-button__toggled'],
-          disabled && STYLES['echo-switch-button__disabled'],
-        )}
-        style={styles?.button}
-      >
+      <span className={cn(button(), classNames?.button)} style={styles?.button}>
         {/* Thumb */}
         <span
-          className={cn(
-            STYLES['echo-switch-thumb'],
-            sizeClassNames.thumb,
-            classNames?.thumb,
-            toggled && STYLES['echo-switch-thumb__toggled'],
-            disabled && STYLES['echo-switch-thumb__disabled'],
-          )}
-          style={styles?.thumb}
+          className={cn(thumb(), classNames?.thumb)}
+          style={{ ...styles?.thumb, left: toggled ? `calc(50% + 0.75rem / 2)` : '' }}
         />
       </span>
 
       {/* Label */}
-      <span
-        className={cn(STYLES['echo-switch-label'], sizeClassNames.label, classNames?.label)}
-        style={styles?.label}
-      >
+      <span className={cn(label(), classNames?.label)} style={styles?.label}>
         {restProps.children}
       </span>
     </label>
