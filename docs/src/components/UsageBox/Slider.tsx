ce13f8715492c35bdbecca2a3be2cf5c4eac4dbf
codeacme17 <vir_kings@qq.com>
2023-12-30 13:20:09 +0800

    docs(Slider): upload slider example component

--- docs/src/components/UsageBox/Slider.tsx
+++ docs/src/components/UsageBox/Slider.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import * as Tone from 'tone'
-import { Slider, Button } from 'echo-ui'
-import { Play, Square } from 'lucide-react'
+import { Slider } from 'echo-ui'
 import { UsageBox } from '.'
+import { UncontrolledSlider } from '../Example/UncontrolledSlider'
 
 export const Default = () => {
   const scope = { Slider }
@@ -51,78 +50,17 @@ export const Step = () => {
   return <UsageBox code={code} scope={scope} classNames={{ preview: 'p-0 px-5 py-10' }} />
 }
 
-const UncontrolledSlider = () => {
-  const url = 'https://codeacme17.github.io/1llest-waveform-vue/audios/loop-1.mp3'
-  const [value, setValue] = React.useState<number>(0)
-  const [player, setPlayer] = React.useState<Tone.Player | null>(null)
-  const [isPlay, setIsPlay] = React.useState(false)
-  const [meter] = React.useState<Tone.Meter>(new Tone.Meter())
-
-  const handlePlay = () => {
-    setIsPlay(!isPlay)
-    if (!player) return
-    player.volume.value = 5
-
-    if (player.state === 'started') {
-      player.stop()
-      return
-    }
-
-    player.start()
-    player.connect(meter)
-    getDB()
-  }
-
-  React.useEffect(() => {
-    const player = new Tone.Player(url).toDestination()
-    setPlayer(player)
-
-    return () => {
-      player.stop()
-      player.disconnect()
-    }
-  }, [])
-
-  const getDB = () => {
-    if (player?.state === 'stopped') {
-      setValue(-60)
-      return
-    }
-    setValue(meter.getValue() as number)
-    requestAnimationFrame(getDB)
-  }
-
-  return (
-    <section className="flex flex-col items-center">
-      <Button onClick={handlePlay} disabled={!player} toggled={isPlay} className="mb-5 px-4">
-        {isPlay ? (
-          <Square className="w-4 h-4 fill-current" />
-        ) : (
-          <Play className="w-4 h-4 fill-current" />
-        )}
-      </Button>
-
-      <div className="h-80">
-        <Slider
-          className="w-2"
-          hideThumb
-          prohibitInteraction
-          vertical
-          axis
-          min={-60}
-          max={10}
-          value={value}
-        />
-      </div>
-    </section>
-  )
-}
-
 export const Uncontrolled = () => {
-  const scope = { UncontrolledSlider, Tone, Button, Play, Square }
+  const scope = { UncontrolledSlider }
   const code = `<UncontrolledSlider />`
 
   return (
-    <UsageBox code={code} scope={scope} type="link" classNames={{ preview: 'p-0 px-5 py-10' }} />
+    <UsageBox
+      code={code}
+      scope={scope}
+      type="link"
+      url="https://github.com/codeacme17/echo-ui/blob/bc2bccb9312a030ff1ffc8517349bf543e6fa3da/example/src/components/controls/UncontrolledSlider.tsx#L6"
+      classNames={{ preview: 'p-0 px-5 py-10' }}
+    />
   )
 }
