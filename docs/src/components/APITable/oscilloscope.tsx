7425427d5b362030d701d45efa4bdeaa027684e4
codeacme17 <vir_kings@qq.com>
2024-01-09 18:49:05 +0800

    docs(Osci): update oscilloscope page

---
+++ docs/src/components/APITable/oscilloscope.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { APITable, APITableDataType } from '.'
+import { Code } from '@nextui-org/react'
+
+export const OscilloscopeAPITable = () => {
+  const data: APITableDataType[] = [
+    {
+      attribute: 'data*',
+      description: '传递给示波器的数组数据',
+      type: <Code>OscilloscopeDataPoint[]</Code>,
+      default: '-',
+    },
+    {
+      attribute: 'amplitudeRange',
+      description: '示波器的振幅范围',
+      type: <Code>[number, number]</Code>,
+      default: <Code> [-2, 2]</Code>,
+    },
+    {
+      attribute: 'lineColor',
+      description: '示波器的线条颜色',
+      type: <Code>string</Code>,
+      default: <Code>'var(--echo-primary)'</Code>,
+    },
+    {
+      attribute: 'lineWidth',
+      description: '示波器的线条宽度',
+      type: <Code>number</Code>,
+      default: <Code>3</Code>,
+    },
+  ]
+
+  return <APITable data={data} />
+}
--- docs/src/components/UsageBox/Oscilloscope.tsx
+++ docs/src/components/UsageBox/Oscilloscope.tsx
@@ -6,5 +6,12 @@ export const Defualt = () => {
   const scope = { OscilloscopeDefault }
   const code = `<OscilloscopeDefault />`
 
-  return <UsageBox code={code} scope={scope} type="link" />
+  return (
+    <UsageBox
+      code={code}
+      scope={scope}
+      type="link"
+      url="https://github.com/codeacme17/echo-ui/blob/main/docs/src/components/Example/OscilloscopeDefault.tsx"
+    />
+  )
 }
