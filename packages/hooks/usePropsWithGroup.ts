91bce453dd44b49264766155f94278fb9229732b
leyoonafr <vir_kings@qq.com>
2024-01-09 11:26:12 +0800

    refactor: update `usePropsWithGroup` comments

--- packages/hooks/usePropsWithGroup.ts
+++ packages/hooks/usePropsWithGroup.ts
@@ -1,6 +1,13 @@
 /**
  * A custom hook that combines properties from a component's props and a group context,
  * with an optional list of keys prioritizing values from the group context.
+ *
+ * @template P - The type of the component's props.
+ * @template G - The type of the group context.
+ * @param {P} props - The component's props.
+ * @param {G | null} groupContext - The group context, which can be null.
+ * @param {string[] | undefined} groupPriorKeys - An optional list of keys prioritizing values from the group context.
+ * @returns {P} - A merged set of properties from props and group context, with optional prioritization.
  */
 
 export const usePropsWithGroup = <P extends Record<string, any>, G extends Record<string, any>>(
