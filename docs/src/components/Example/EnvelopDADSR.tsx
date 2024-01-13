98a07a40df32133883cd22f4ad8b63a0c05ed228
codeacme17 <codeacme17@gmail.com>
2024-01-13 15:50:45 +0800

    docs: update Envelop

--- docs/src/components/Example/EnvelopDADSR.tsx
+++ docs/src/components/Example/EnvelopDADSR.tsx
@@ -1,10 +1,11 @@
 import * as Tone from 'tone'
 import * as React from 'react'
 import { Envelope, EnvelopeData, Knob, Button } from 'echo-ui'
-import { Hand } from 'lucide-react'
+import { Activity } from 'lucide-react'
 
-export const EnvelopDefault = () => {
+export const EnvelopDADSR = () => {
   const envelopeData: EnvelopeData = {
+    delay: 0.1,
     attack: 0.6,
     decay: 0.2,
     sustain: 0.8,
@@ -12,17 +13,18 @@ export const EnvelopDefault = () => {
   }
 
   const [data, setData] = React.useState({ ...envelopeData })
+  const [delay, setDelay] = React.useState(envelopeData.delay)
   const [attack, setAttack] = React.useState(envelopeData.attack)
   const [decay, setDecay] = React.useState(envelopeData.decay)
   const [sustain, setSustain] = React.useState(envelopeData.sustain)
   const [release, setRelease] = React.useState(envelopeData.release)
-  const [isPlaying, setIsPlaying] = React.useState(false)
 
   React.useEffect(() => {
-    setData({ attack, decay, sustain, release })
-  }, [attack, decay, sustain, release])
+    setData({ delay, attack, decay, sustain, release })
+  }, [delay, attack, decay, sustain, release])
 
   const handleDataChange = (data: EnvelopeData) => {
+    setDelay(data.delay)
     setAttack(data.attack)
     setDecay(data.decay)
     setSustain(data.sustain)
@@ -33,7 +35,6 @@ export const EnvelopDefault = () => {
   const oscillator = React.useRef<Tone.Oscillator>()
 
   const handleMouseDown = () => {
-    setIsPlaying(true)
     envelope.current = new Tone.AmplitudeEnvelope({
       attack,
       decay,
@@ -41,31 +42,14 @@ export const EnvelopDefault = () => {
       release,
     }).toDestination()
     oscillator.current = new Tone.Oscillator().connect(envelope.current).start()
-    envelope.current.triggerAttack()
-  }
-
-  const handleMouseUp = () => {
-    setIsPlaying(false)
-    if (!envelope.current) return
-    envelope.current.triggerRelease()
-
-    setTimeout(() => {
-      if (!envelope.current || !oscillator.current) return
-      oscillator.current.stop()
-      oscillator.current.dispose()
-      envelope.current.dispose()
-    }, release * 1000)
+    envelope.current.triggerAttack(`+${delay || 0}`)
+    envelope.current.triggerRelease(`+${delay! + attack + decay}}`)
   }
 
   return (
-    <section className="flex flex-col items-center w-full">
-      <Button
-        className="mb-5 cursor-grab"
-        toggled={isPlaying}
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-      >
-        <Hand className="w-4 h-4" />
+    <section className="flex flex-col items-center">
+      <Button className="mb-5" onMouseDown={handleMouseDown}>
+        <Activity className="w-4 h-4" />
       </Button>
 
       <Envelope data={data} onDataChange={handleDataChange} />
@@ -82,6 +66,7 @@ export const EnvelopDefault = () => {
         step={0.01}
         sensitivity={5}
       >
+        <Knob bottomLabel="Delay" value={delay} onChange={setDelay} />
         <Knob bottomLabel="Attack" value={attack} onChange={setAttack} />
         <Knob bottomLabel="Decay" value={decay} onChange={setDecay} />
         <Knob bottomLabel="Sustain" value={sustain} onChange={setSustain} />
--- docs/src/components/UsageBox/Envelop.tsx
+++ docs/src/components/UsageBox/Envelop.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import { EnvelopDefault } from '../Example/EnvelopDefault'
+import { EnvelopAHDSR } from '../Example/EnvelopAHDSR'
+import { EnvelopDADSR } from '../Example/EnvelopDADSR'
 import { UsageBox } from '.'
 
 export const Default = () => {
   const scope = { EnvelopDefault }
   const code = `<EnvelopDefault />`
+
+  return <UsageBox code={code} scope={scope} />
+}
+
+export const AHDSR = () => {
+  const scope = { EnvelopAHDSR }
+  const code = `<EnvelopAHDSR />`
+
+  return <UsageBox code={code} scope={scope} />
+}
+
+export const DADSR = () => {
+  const scope = { EnvelopDADSR }
+  const code = `<EnvelopDADSR />`
 
   return <UsageBox code={code} scope={scope} />
 }
