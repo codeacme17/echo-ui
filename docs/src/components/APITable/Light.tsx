7187005ab08a966b4956cc3ce4bef7cee2ea83a8
codeacme17 <vir_kings@qq.com>
2024-01-14 18:34:42 +0800

    docs(Light): update en doc

--- docs/src/components/APITable/Light.tsx
+++ docs/src/components/APITable/Light.tsx
@@ -6,19 +6,19 @@ export const LightAPITable = () => {
   const data: APITableDataType[] = [
     {
       attribute: 'on',
-      description: '指示灯是否开启',
+      description: 'Whether the indicator light is on',
       type: <Code>boolean</Code>,
       default: <Code>false</Code>,
     },
     {
       attribute: 'size',
-      description: '指示灯的大小',
-      type: <Code> number | string </Code>,
+      description: 'Size of the indicator light',
+      type: <Code>number | string</Code>,
       default: <Code>0.75rem</Code>,
     },
     {
       attribute: 'color',
-      description: '指示灯灯光颜色',
+      description: 'Color of the indicator light',
       type: <Code>string</Code>,
       default: <Code>var(--echo-primary)</Code>,
     },
