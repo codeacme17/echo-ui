e885234c54913cf05dcc079f53eb768b5fe80dce
codeacme17 <vir_kings@qq.com>
2024-01-14 16:02:01 +0800

    chore(docs): refactor build step

--- docs/.island/dist/assets/button.a0cd58fc.js
+++
@@ -1,5 +0,0 @@
-import{jsx as e,jsxs as o,Fragment as a}from"react/jsx-runtime";const s=void 0,i=[],d="Button";function c(n){const t=Object.assign({h1:"h1",a:"a",p:"p"},n.components);return o(a,{children:[o(t.h1,{id:"button",children:[e(t.a,{className:"header-anchor","aria-hidden":"true",href:"#button",children:"#"}),"Button"]}),`
-`,e(t.p,{children:"123123"})]})}function u(n={}){const{wrapper:t}=n.components||{};return t?e(t,Object.assign({},n,{children:e(c,n)})):c(n)}const h="2023/12/27 19:55:35",l=`# Button
-
-123123
-`;export{l as content,u as default,s as frontmatter,h as lastUpdatedTime,d as title,i as toc};
