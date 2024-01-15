8e1edc6519fcece91ddfab107feaf491469c69c4
codeacme17 <vir_kings@qq.com>
2024-01-15 18:45:10 +0800

    docs(Envelope): update api and type

---
+++ docs/src/components/APITable/Envelope.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { APITable, APITableDataType } from '.'
+import { Code } from '@nextui-org/react'
+
+export const EnvelopeAPITable = () => {
+  const data: APITableDataType[] = [
+    {
+      attribute: 'data*',
+      description: 'The ADSP data to be displayed(required)',
+      type: <Code>EnvelopeData</Code>,
+      default: '-',
+    },
+    {
+      attribute: 'limits',
+      description: 'The limits of each ADSP data',
+      type: <Code>EnvelopeLimits</Code>,
+      default: <Code>0.75rem</Code>,
+    },
+    {
+      attribute: 'lineColor',
+      description: 'Color of the line',
+      type: <Code>string</Code>,
+      default: <Code>var(--echo-primary)</Code>,
+    },
+    {
+      attribute: 'lineWidth',
+      description: 'Width of the line',
+      type: <Code>number</Code>,
+      default: <Code>3</Code>,
+    },
+    {
+      attribute: 'nodeColor',
+      description: 'Color of the node',
+      type: <Code>string</Code>,
+      default: <Code>var(--echo-secondary)</Code>,
+    },
+    {
+      attribute: 'nodeSize',
+      description: 'Size of the node',
+      type: <Code>number</Code>,
+      default: <Code>6</Code>,
+    },
+    {
+      attribute: 'onDataChange',
+      description: 'The callback function when data changed',
+      type: <Code>{`(data: EnvelopeData) => void`}</Code>,
+      default: '-',
+    },
+  ]
+
+  return <APITable data={data} />
+}
