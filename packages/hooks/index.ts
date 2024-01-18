d47723e3c30ab85dbdbf98c15f8702ff2d4cb9fd
codeacme17 <vir_kings@qq.com>
2024-01-18 22:27:49 +0800

    feat(hook): add `getValue` output at `usevuMeter`

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useFetchAudio, type UseFetchAudioProps } from './useFetchAudio'
 export { usePlayer, type UsePlayerProps } from './usePlayer'
+export { useVuMeter, type UseVuMeterProps } from './useVuMeter'
 export { useWaveform, type UseWaveformProps } from './useWaveform'
