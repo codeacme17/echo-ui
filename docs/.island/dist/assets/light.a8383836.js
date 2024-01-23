701481fa0144fe1d71bf4bc96294c7556ca69433
codeacme17 <vir_kings@qq.com>
2024-01-23 17:14:02 +0800

    chore(docs): build dist

---
+++ docs/.island/dist/assets/light.a8383836.js
@@ -0,0 +1,71 @@
+import{jsx as n,jsxs as t,Fragment as a}from"react/jsx-runtime";import{D as o,O as d,C as c,S as s,L as l}from"./Light.90d9d362.js";import{C as r}from"./CodeBlock.69bc42b0.js";import"./index.b6b2392c.js";import"react";import"../client-entry.js";import"./chunk-FXLYF44B.9f5da3e9.js";import"react-dom";const C=void 0,T=[{id:"\u5F15\u5165",text:"\u5F15\u5165",depth:2},{id:"\u4EE3\u7801\u6F14\u793A",text:"\u4EE3\u7801\u6F14\u793A",depth:2},{id:"\u5F00\u542F\u72B6\u6001",text:"\u5F00\u542F\u72B6\u6001",depth:3},{id:"\u706F\u5149\u989C\u8272",text:"\u706F\u5149\u989C\u8272",depth:3},{id:"\u5C3A\u5BF8",text:"\u5C3A\u5BF8",depth:3},{id:"api",text:"API",depth:2},{id:"light",text:"Light",depth:3},{id:"\u7C7B\u578B\u58F0\u660E",text:"\u7C7B\u578B\u58F0\u660E",depth:2}],D="Light \u6307\u793A\u706F";function h(i){const e=Object.assign({h1:"h1",a:"a",p:"p",h2:"h2",h3:"h3"},i.components);return t(a,{children:[t(e.h1,{id:"light-\u6307\u793A\u706F",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#light-\u6307\u793A\u706F",children:"#"}),"Light \u6307\u793A\u706F"]}),`
+`,n(e.p,{children:"\u6307\u793A\u706F\u7EC4\u4EF6\u4E3B\u8981\u53EF\u4EE5\u7528\u6765\u5BF9\u67D0\u4E9B\u72B6\u6001\u8FDB\u884C\u6807\u8BB0\uFF0C\u5982\u6548\u679C\u5668\u662F\u5426\u5F00\u542F\u7B49\u72B6\u6001"}),`
+`,t(e.h2,{id:"\u5F15\u5165",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u5F15\u5165",children:"#"}),"\u5F15\u5165"]}),`
+`,n(r,{code:"import { Light } from 'echo-ui'"}),`
+`,t(e.h2,{id:"\u4EE3\u7801\u6F14\u793A",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u4EE3\u7801\u6F14\u793A",children:"#"}),"\u4EE3\u7801\u6F14\u793A"]}),`
+`,n(o,{}),`
+`,t(e.h3,{id:"\u5F00\u542F\u72B6\u6001",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u5F00\u542F\u72B6\u6001",children:"#"}),"\u5F00\u542F\u72B6\u6001"]}),`
+`,n(d,{}),`
+`,t(e.h3,{id:"\u706F\u5149\u989C\u8272",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u706F\u5149\u989C\u8272",children:"#"}),"\u706F\u5149\u989C\u8272"]}),`
+`,n(c,{}),`
+`,t(e.h3,{id:"\u5C3A\u5BF8",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u5C3A\u5BF8",children:"#"}),"\u5C3A\u5BF8"]}),`
+`,n(s,{}),`
+`,t(e.h2,{id:"api",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#api",children:"#"}),"API"]}),`
+`,t(e.h3,{id:"light",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#light",children:"#"}),"Light"]}),`
+`,n(l,{}),`
+`,t(e.h2,{id:"\u7C7B\u578B\u58F0\u660E",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u7C7B\u578B\u58F0\u660E",children:"#"}),"\u7C7B\u578B\u58F0\u660E"]}),`
+`,n(r,{code:`export interface LightProps extends React.HTMLAttributes<HTMLDivElement> {
+  on?: boolean
+  size?: number | string
+  color?: string
+}
+
+export interface LightRef extends HTMLDivElement {}`})]})}function A(i={}){const{wrapper:e}=i.components||{};return e?n(e,Object.assign({},i,{children:n(h,i)})):h(i)}const M="2024/1/10 10:10:15",N=`import {
+  DefaultLight,
+  OnLight,
+  ColorLight,
+  SizeLight,
+} from '../../src/components/UsageBox/Light.tsx'
+import { LightAPITable } from '../../src/components/APITable/Light.tsx'
+import { CodeBlock } from '../../src/components/CodeBlock.tsx'
+
+# Light \u6307\u793A\u706F
+
+\u6307\u793A\u706F\u7EC4\u4EF6\u4E3B\u8981\u53EF\u4EE5\u7528\u6765\u5BF9\u67D0\u4E9B\u72B6\u6001\u8FDB\u884C\u6807\u8BB0\uFF0C\u5982\u6548\u679C\u5668\u662F\u5426\u5F00\u542F\u7B49\u72B6\u6001
+
+## \u5F15\u5165
+
+<CodeBlock code={\`import { Light } from 'echo-ui'\`} />
+
+## \u4EE3\u7801\u6F14\u793A
+
+<DefaultLight />
+
+### \u5F00\u542F\u72B6\u6001
+
+<OnLight />
+
+### \u706F\u5149\u989C\u8272
+
+<ColorLight />
+
+### \u5C3A\u5BF8
+
+<SizeLight />
+
+## API
+
+### Light
+
+<LightAPITable />
+
+## \u7C7B\u578B\u58F0\u660E
+
+<CodeBlock code={\`export interface LightProps extends React.HTMLAttributes<HTMLDivElement> {
+  on?: boolean
+  size?: number | string
+  color?: string
+}
+
+export interface LightRef extends HTMLDivElement {}\`} />
+`;export{N as content,A as default,C as frontmatter,M as lastUpdatedTime,D as title,T as toc};
---
+++ docs/.island/dist/assets/oscilloscope.43f21af6.js
@@ -0,0 +1,59 @@
+import{jsx as n,jsxs as t,Fragment as r}from"react/jsx-runtime";import{C as i}from"./CodeBlock.69bc42b0.js";import{D as c,O as s}from"./Oscilloscope.f406bf8e.js";import"react";import"./index.b6b2392c.js";import"../client-entry.js";import"./chunk-FXLYF44B.9f5da3e9.js";import"react-dom";import"./index.194ce9d9.js";import"./Analyser.525c4ea2.js";const b=void 0,D=[{id:"import",text:"Import",depth:2},{id:"usage",text:"Usage",depth:2},{id:"api",text:"API",depth:2},{id:"type-declarations",text:"Type Declarations",depth:2}],P="Oscilloscope";function a(o){const e=Object.assign({h1:"h1",a:"a",p:"p",h2:"h2"},o.components);return t(r,{children:[t(e.h1,{id:"oscilloscope",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#oscilloscope",children:"#"}),"Oscilloscope"]}),`
+`,n(e.p,{children:"Real-time visualization of audio waveform, displaying changes in signal amplitude and frequency over time."}),`
+`,t(e.h2,{id:"import",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#import",children:"#"}),"Import"]}),`
+`,n(i,{code:"import { Oscilloscope } from 'echo-ui'"}),`
+`,t(e.h2,{id:"usage",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#usage",children:"#"}),"Usage"]}),`
+`,n(c,{}),`
+`,t(e.h2,{id:"api",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#api",children:"#"}),"API"]}),`
+`,n(s,{}),`
+`,t(e.h2,{id:"type-declarations",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#type-declarations",children:"#"}),"Type Declarations"]}),`
+`,n(i,{code:`export interface OscilloscopeProps extends React.HTMLAttributes<OscilloscopeRef> {
+  data: OscilloscopeDataPoint[]
+  amplitudeRange?: [number, number]
+  lineColor?: string
+  lineWidth?: number
+}
+
+export interface OscilloscopeDataPoint {
+  index: number
+  amplitude: number
+}
+
+export interface OscilloscopeRef extends HTMLDivElement {}
+`})]})}function T(o={}){const{wrapper:e}=o.components||{};return e?n(e,Object.assign({},o,{children:n(a,o)})):a(o)}const y="2024/1/14 18:46:31",C=`import { CodeBlock } from '../../src/components/CodeBlock.tsx'
+import { Defualt } from '../../src/components/UsageBox/Oscilloscope.tsx'
+import { OscilloscopeAPITable } from '../../src/components/APITable/Oscilloscope.tsx'
+
+# Oscilloscope
+
+Real-time visualization of audio waveform, displaying changes in signal amplitude and frequency over time.
+
+## Import
+
+<CodeBlock code={\`import { Oscilloscope } from 'echo-ui'\`} />
+
+## Usage
+
+<Defualt />
+
+## API
+
+<OscilloscopeAPITable />
+
+## Type Declarations
+
+<CodeBlock code={\`export interface OscilloscopeProps extends React.HTMLAttributes<OscilloscopeRef> {
+  data: OscilloscopeDataPoint[]
+  amplitudeRange?: [number, number]
+  lineColor?: string
+  lineWidth?: number
+}
+
+export interface OscilloscopeDataPoint {
+  index: number
+  amplitude: number
+}
+
+export interface OscilloscopeRef extends HTMLDivElement {}
+\`} />
+`;export{C as content,T as default,b as frontmatter,y as lastUpdatedTime,P as title,D as toc};
