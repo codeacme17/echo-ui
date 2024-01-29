6840fd0d9765ee8024d6e596b778bea3e52e728a
leyoonafr <vir_kings@qq.com>
2024-01-29 16:10:51 +0800

    docs(example): update `OscilloscopeDefault`

--- docs/src/components/Example/OscilloscopeDefault.tsx
+++ docs/src/components/Example/OscilloscopeDefault.tsx
@@ -1,68 +1,38 @@
-import * as Tone from 'tone'
 import * as React from 'react'
-import { Oscilloscope, OscilloscopeDataPoint, Button } from 'echo-ui'
+import { Oscilloscope, Button, useFetchAudio, usePlayer, useOscilloscope } from 'echo-ui'
 
 export const OscilloscopeDefault = () => {
   const url = '/audios/loop-5.mp3'
-  const [data, setData] = React.useState<OscilloscopeDataPoint[]>([])
-  const [trigger, setTrigger] = React.useState(false)
-  const analyser = React.useRef<Tone.Analyser>()
-  const player = React.useRef<Tone.Player | null>(null)
-
-  const fftSize = 512 * 2
+  const { audioBuffer, fetchAudio } = useFetchAudio({ url })
+  const { init: initPlayer, isPlaying, play, pause } = usePlayer()
+  const { observer, cancelObserve, analyser, init: initOscilloscope, data } = useOscilloscope()
 
   React.useEffect(() => {
-    player.current = new Tone.Player(url)
-    analyser.current = new Tone.Analyser('waveform', fftSize)
-    player.current.toDestination()
-
-    return () => {
-      player.current?.disconnect()
-      player.current?.dispose()
-    }
+    fetchAudio()
+    initOscilloscope()
   }, [])
 
-  const handleTrigger = async () => {
-    if (!player.current || !analyser.current) {
-      console.error('Oscillator or Analyser is not initialized')
-      return
-    }
+  React.useEffect(() => {
+    if (!audioBuffer || !analyser) return
+    initPlayer(audioBuffer, [analyser])
+  }, [audioBuffer, analyser])
 
-    if (trigger) {
-      player.current.stop()
-      cancelAnimationFrame(requestId.current)
-      setData([])
-      setTrigger(false)
+  const handleTrigger = () => {
+    if (isPlaying) {
+      pause()
+      cancelObserve()
     } else {
-      player.current?.connect(analyser.current)
-      player.current.loop = true
-      player.current.start()
-      setTrigger(true)
-      getData()
+      play()
+      observer()
     }
-  }
-
-  const requestId = React.useRef<number>(0)
-
-  const getData = () => {
-    const spectrumData = analyser.current?.getValue()
-
-    if (spectrumData instanceof Float32Array) {
-      const formattedData = Array.from(spectrumData).map((amplitude, index) => {
-        return { index, amplitude }
-      })
-      setData(formattedData)
-    }
-
-    requestId.current = requestAnimationFrame(getData)
   }
 
   return (
-    <section className="flex flex-col items-center gap-2">
-      <Oscilloscope className="w-full" data={data} />
+    <section className="flex flex-col items-center gap-2 w-full">
+      <Oscilloscope className="w-full" data={data} amplitudeRange={[-5, 5]} />
 
-      <Button onClick={handleTrigger} toggled={trigger}>
-        {trigger ? 'Stop' : 'Start'}
+      <Button onClick={handleTrigger} toggled={isPlaying}>
+        {isPlaying ? 'Stop' : 'Start'}
       </Button>
     </section>
   )
