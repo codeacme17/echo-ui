2bbbcea46a27c23ee69c317ae4bc88908e10fef1
leyoonafr <vir_kings@qq.com>
2024-01-29 17:19:29 +0800

    docs: build dist

---
+++ docs/.island/dist/assets/button.69c413bd.js
@@ -0,0 +1,143 @@
+import{jsx as t,jsxs as e,Fragment as d}from"react/jsx-runtime";import{D as i,T as c,a as u,S as h,R as l,G as s,B as p,b as B}from"./Button.6b862713.js";import{C as o}from"./CodeBlock.de027be4.js";import"./index.c7334c27.js";import"./index.2d091e0a.js";import"react";import"../client-entry.js";import"./chunk-FXLYF44B.3b11858b.js";import"react-dom";const G=void 0,N=[{id:"\u5F15\u5165",text:"\u5F15\u5165",depth:2},{id:"\u4EE3\u7801\u6F14\u793A",text:"\u4EE3\u7801\u6F14\u793A",depth:2},{id:"\u5207\u6362\u72B6\u6001",text:"\u5207\u6362\u72B6\u6001",depth:3},{id:"\u7981\u7528\u72B6\u6001",text:"\u7981\u7528\u72B6\u6001",depth:3},{id:"\u5C3A\u5BF8",text:"\u5C3A\u5BF8",depth:3},{id:"\u5706\u89D2",text:"\u5706\u89D2",depth:3},{id:"\u6309\u94AE\u7EC4",text:"\u6309\u94AE\u7EC4",depth:3},{id:"\u6807\u7B7E-data",text:"\u6807\u7B7E Data",depth:2},{id:"api",text:"API",depth:2},{id:"button",text:"Button",depth:3},{id:"buttongroup",text:"Button.Group",depth:3},{id:"\u7C7B\u578B\u58F0\u660E",text:"\u7C7B\u578B\u58F0\u660E",depth:2}],C="Button \u6309\u94AE";function a(r){const n=Object.assign({h1:"h1",a:"a",p:"p",code:"code",h2:"h2",h3:"h3",ul:"ul",li:"li",div:"div"},r.components);return e(d,{children:[e(n.h1,{id:"button-\u6309\u94AE",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#button-\u6309\u94AE",children:"#"}),"Button \u6309\u94AE"]}),`
+`,e(n.p,{children:["\u53EF\u4EE5\u4F7F\u7528 ",t(n.code,{children:"Button"})," \u7EC4\u4EF6\u6765\u521B\u5EFA\u4E00\u4E2A\u6309\u94AE\uFF0C\u4E5F\u53EF\u4EE5\u4F7F\u7528 ",t(n.code,{children:"Button.Group"})," \u7EC4\u4EF6\u6765\u521B\u5EFA\u4E00\u7EC4\u6309\u94AE\uFF0C\u4ECE\u800C\u5B9E\u73B0\u5BF9\u97F3\u9891\u64AD\u653E\u7B49\u529F\u80FD\u7684\u63A7\u5236"]}),`
+`,e(n.h2,{id:"\u5F15\u5165",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#\u5F15\u5165",children:"#"}),"\u5F15\u5165"]}),`
+`,t(o,{code:"import { Button } from 'echo-ui'"}),`
+`,e(n.h2,{id:"\u4EE3\u7801\u6F14\u793A",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#\u4EE3\u7801\u6F14\u793A",children:"#"}),"\u4EE3\u7801\u6F14\u793A"]}),`
+`,t(i,{}),`
+`,e(n.h3,{id:"\u5207\u6362\u72B6\u6001",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#\u5207\u6362\u72B6\u6001",children:"#"}),"\u5207\u6362\u72B6\u6001"]}),`
+`,t(c,{}),`
+`,e(n.h3,{id:"\u7981\u7528\u72B6\u6001",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#\u7981\u7528\u72B6\u6001",children:"#"}),"\u7981\u7528\u72B6\u6001"]}),`
+`,t(u,{}),`
+`,e(n.h3,{id:"\u5C3A\u5BF8",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#\u5C3A\u5BF8",children:"#"}),"\u5C3A\u5BF8"]}),`
+`,t(h,{}),`
+`,e(n.h3,{id:"\u5706\u89D2",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#\u5706\u89D2",children:"#"}),"\u5706\u89D2"]}),`
+`,t(l,{}),`
+`,e(n.h3,{id:"\u6309\u94AE\u7EC4",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#\u6309\u94AE\u7EC4",children:"#"}),"\u6309\u94AE\u7EC4"]}),`
+`,t(s,{}),`
+`,e(n.h2,{id:"\u6807\u7B7E-data",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#\u6807\u7B7E-data",children:"#"}),"\u6807\u7B7E Data"]}),`
+`,e(n.ul,{children:[`
+`,e(n.li,{children:[`
+`,e(n.p,{children:[t(n.code,{children:"data-toggled"}),": \u5F53\u524D\u6309\u94AE\u662F\u5426\u5904\u4E8E\u6FC0\u6D3B\u72B6\u6001"]}),`
+`]}),`
+`,e(n.li,{children:[`
+`,e(n.p,{children:[t(n.code,{children:"data-disabled"}),": \u5F53\u524D\u6309\u94AE\u662F\u5426\u5904\u4E8E\u7981\u7528\u72B6\u6001"]}),`
+`]}),`
+`]}),`
+`,e(n.h2,{id:"api",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#api",children:"#"}),"API"]}),`
+`,e(n.h3,{id:"button",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#button",children:"#"}),"Button"]}),`
+`,e(n.div,{className:"island-directive tip",children:[t(n.p,{className:"island-directive-title",children:"TIP"}),t(n.div,{className:"island-directive-content",children:e(n.p,{children:["\u652F\u6301\u539F\u751F ",t(n.code,{children:"button"})," \u7684\u5176\u4ED6\u6240\u6709\u5C5E\u6027"]})})]}),`
+`,t(p,{}),`
+`,e(n.h3,{id:"buttongroup",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#buttongroup",children:"#"}),"Button.Group"]}),`
+`,t(B,{}),`
+`,e(n.h2,{id:"\u7C7B\u578B\u58F0\u660E",children:[t(n.a,{className:"header-anchor","aria-hidden":"true",href:"#\u7C7B\u578B\u58F0\u660E",children:"#"}),"\u7C7B\u578B\u58F0\u660E"]}),`
+`,t(o,{code:`interface AbstractButtonProps<T> extends React.HTMLAttributes<T> {
+  value?: any
+  disabled?: boolean
+  size?: 'sm' | 'md' | 'lg'
+  radius?: 'none' | 'sm' | 'md' | 'lg' | 'full'
+}
+
+export interface ButtonProps extends AbstractButtonProps<ButtonRef> {
+  toggled?: boolean
+  onToggleChange?: (toggled: boolean) => void
+}
+
+export interface ButtonGroupProps extends Omit<AbstractButtonProps<ButtonGroupRef>, 'onChange'> {
+  value?: any[]
+  classNames?: { button?: string }
+  styles?: { button?: React.CSSProperties }
+  onChange?: (values: any[]) => void
+}
+
+export interface ButtonRef extends HTMLButtonElement {}
+
+export interface ButtonGroupRef extends HTMLDivElement {}`})]})}function R(r={}){const{wrapper:n}=r.components||{};return n?t(n,Object.assign({},r,{children:t(a,r)})):a(r)}const D="2024/1/10 10:10:15",I=`import {\r
+  DefaultButton,\r
+  ToggledButton,\r
+  DisabledButton,\r
+  SizeButton,\r
+  RadiusButton,\r
+  GroupButton,\r
+} from '../../src/components/UsageBox/Button'\r
+import { ButtonAPITable, ButtonGroupAPITable } from '../../src/components/APITable/Button.tsx'\r
+import { CodeBlock } from '../../src/components/CodeBlock.tsx'\r
+\r
+# Button \u6309\u94AE\r
+\r
+\u53EF\u4EE5\u4F7F\u7528 \`Button\` \u7EC4\u4EF6\u6765\u521B\u5EFA\u4E00\u4E2A\u6309\u94AE\uFF0C\u4E5F\u53EF\u4EE5\u4F7F\u7528 \`Button.Group\` \u7EC4\u4EF6\u6765\u521B\u5EFA\u4E00\u7EC4\u6309\u94AE\uFF0C\u4ECE\u800C\u5B9E\u73B0\u5BF9\u97F3\u9891\u64AD\u653E\u7B49\u529F\u80FD\u7684\u63A7\u5236\r
+\r
+## \u5F15\u5165\r
+\r
+<CodeBlock code={\`import { Button } from 'echo-ui'\`} />\r
+\r
+## \u4EE3\u7801\u6F14\u793A\r
+\r
+<DefaultButton />\r
+\r
+### \u5207\u6362\u72B6\u6001\r
+\r
+<ToggledButton />\r
+\r
+### \u7981\u7528\u72B6\u6001\r
+\r
+<DisabledButton />\r
+\r
+### \u5C3A\u5BF8\r
+\r
+<SizeButton />\r
+\r
+### \u5706\u89D2\r
+\r
+<RadiusButton />\r
+\r
+### \u6309\u94AE\u7EC4\r
+\r
+<GroupButton />\r
+\r
+## \u6807\u7B7E Data\r
+\r
+- \`data-toggled\`: \u5F53\u524D\u6309\u94AE\u662F\u5426\u5904\u4E8E\u6FC0\u6D3B\u72B6\u6001\r
+\r
+- \`data-disabled\`: \u5F53\u524D\u6309\u94AE\u662F\u5426\u5904\u4E8E\u7981\u7528\u72B6\u6001\r
+\r
+## API\r
+\r
+### Button\r
+\r
+:::tip\r
+\r
+\u652F\u6301\u539F\u751F \`button\` \u7684\u5176\u4ED6\u6240\u6709\u5C5E\u6027\r
+\r
+:::\r
+\r
+<ButtonAPITable />\r
+\r
+### Button.Group\r
+\r
+<ButtonGroupAPITable />\r
+\r
+## \u7C7B\u578B\u58F0\u660E\r
+\r
+<CodeBlock code={\`interface AbstractButtonProps<T> extends React.HTMLAttributes<T> {\r
+  value?: any\r
+  disabled?: boolean\r
+  size?: 'sm' | 'md' | 'lg'\r
+  radius?: 'none' | 'sm' | 'md' | 'lg' | 'full'\r
+}\r
+\r
+export interface ButtonProps extends AbstractButtonProps<ButtonRef> {\r
+  toggled?: boolean\r
+  onToggleChange?: (toggled: boolean) => void\r
+}\r
+\r
+export interface ButtonGroupProps extends Omit<AbstractButtonProps<ButtonGroupRef>, 'onChange'> {\r
+  value?: any[]\r
+  classNames?: { button?: string }\r
+  styles?: { button?: React.CSSProperties }\r
+  onChange?: (values: any[]) => void\r
+}\r
+\r
+export interface ButtonRef extends HTMLButtonElement {}\r
+\r
+export interface ButtonGroupRef extends HTMLDivElement {}\`} />\r
+`;export{I as content,R as default,G as frontmatter,D as lastUpdatedTime,C as title,N as toc};
---
+++ docs/.island/dist/assets/waveform.e1942a7b.js
@@ -0,0 +1,90 @@
+import{jsx as r,jsxs as n,Fragment as i}from"react/jsx-runtime";import{D as m,W as s}from"./Waveform.5d59edac.js";import{C as a}from"./CodeBlock.de027be4.js";import"react";import"./index.c7334c27.js";import"./index.2d091e0a.js";import"../client-entry.js";import"./chunk-FXLYF44B.3b11858b.js";import"react-dom";import"./square.e4e6b4e9.js";const W=void 0,x=[{id:"import",text:"Import",depth:2},{id:"usage",text:"Usage",depth:2},{id:"api",text:"API",depth:2},{id:"waveform",text:"Waveform",depth:3},{id:"type-declarations",text:"Type Declarations",depth:2}],g="Waveform";function t(o){const e=Object.assign({h1:"h1",a:"a",p:"p",h2:"h2",h3:"h3"},o.components);return n(i,{children:[n(e.h1,{id:"waveform",children:[r(e.a,{className:"header-anchor","aria-hidden":"true",href:"#waveform",children:"#"}),"Waveform"]}),`
+`,r(e.p,{children:"Waveform component can be used to display audio waveform"}),`
+`,n(e.h2,{id:"import",children:[r(e.a,{className:"header-anchor","aria-hidden":"true",href:"#import",children:"#"}),"Import"]}),`
+`,r(a,{code:"import { Waveform } from 'echo-ui'"}),`
+`,n(e.h2,{id:"usage",children:[r(e.a,{className:"header-anchor","aria-hidden":"true",href:"#usage",children:"#"}),"Usage"]}),`
+`,r(m,{}),`
+`,n(e.h2,{id:"api",children:[r(e.a,{className:"header-anchor","aria-hidden":"true",href:"#api",children:"#"}),"API"]}),`
+`,n(e.h3,{id:"waveform-1",children:[r(e.a,{className:"header-anchor","aria-hidden":"true",href:"#waveform-1",children:"#"}),"Waveform"]}),`
+`,r(s,{}),`
+`,n(e.h2,{id:"type-declarations",children:[r(e.a,{className:"header-anchor","aria-hidden":"true",href:"#type-declarations",children:"#"}),"Type Declarations"]}),`
+`,r(a,{code:`export interface WaveformProps
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onMouseMove' | 'onClick'> {
+  data: number[] | number[][]
+  audioDuration: number
+  percentage?: number
+  hideCursor?: boolean
+  cursorWidth?: number
+  cursorColor?: string
+  hideCursorLabel?: boolean
+  disableAnimation?: boolean
+  animationDuration?: number
+  waveHeight?: number
+  waveColor?: string
+  maskColor?: string
+  onClick?: (e: WaveformMouseEvent) => void
+  onMouseMove?: (e: WaveformMouseEvent) => void
+  onMouseLeave?: (e: React.MouseEvent) => void
+}
+
+export interface WaveformMouseEvent {
+  time: number
+  percentage: number
+  nativeEvent: React.MouseEvent
+}
+
+export interface WaveformRef extends HTMLDivElement {}
+
+`})]})}function C(o={}){const{wrapper:e}=o.components||{};return e?r(e,Object.assign({},o,{children:r(t,o)})):t(o)}const D="2024/1/22 10:38:20",E=`import { Default } from '../../src/components/UsageBox/Waveform.tsx'\r
+import { WaveformAPITable } from '../../src/components/APITable/Waveform.tsx'\r
+import { CodeBlock } from '../../src/components/CodeBlock.tsx'\r
+\r
+# Waveform\r
+\r
+Waveform component can be used to display audio waveform\r
+\r
+## Import\r
+\r
+<CodeBlock code={\`import { Waveform } from 'echo-ui'\`} />\r
+\r
+## Usage\r
+\r
+<Default />\r
+\r
+## API\r
+\r
+### Waveform\r
+\r
+<WaveformAPITable />\r
+\r
+## Type Declarations\r
+\r
+<CodeBlock code={\`export interface WaveformProps\r
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onMouseMove' | 'onClick'> {\r
+  data: number[] | number[][]\r
+  audioDuration: number\r
+  percentage?: number\r
+  hideCursor?: boolean\r
+  cursorWidth?: number\r
+  cursorColor?: string\r
+  hideCursorLabel?: boolean\r
+  disableAnimation?: boolean\r
+  animationDuration?: number\r
+  waveHeight?: number\r
+  waveColor?: string\r
+  maskColor?: string\r
+  onClick?: (e: WaveformMouseEvent) => void\r
+  onMouseMove?: (e: WaveformMouseEvent) => void\r
+  onMouseLeave?: (e: React.MouseEvent) => void\r
+}\r
+\r
+export interface WaveformMouseEvent {\r
+  time: number\r
+  percentage: number\r
+  nativeEvent: React.MouseEvent\r
+}\r
+\r
+export interface WaveformRef extends HTMLDivElement {}\r
+\r
+\`} />\r
+`;export{E as content,C as default,W as frontmatter,D as lastUpdatedTime,g as title,x as toc};
