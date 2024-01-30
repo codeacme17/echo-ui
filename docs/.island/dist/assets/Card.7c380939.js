a6b6955d3a06bb82258bbf4672981e0cdff1db8f
leyoonafr <vir_kings@qq.com>
2024-01-30 09:09:44 +0800

    docs: build dist

---
+++ docs/.island/dist/assets/Card.7c380939.js
@@ -0,0 +1,13 @@
+import{E as t,a as n,$ as C,z as b,p as f,b as x,U as l,A as y,c as i}from"./index.39708e08.js";import*as r from"react";import{jsxs as o,jsx as e}from"react/jsx-runtime";const v=()=>{const[a,s]=r.useState(3),[d,u]=r.useState(!1),m=h=>{s(h)},g=()=>{u(!d)},[c,p]=r.useState(!1);return o("section",{className:"flex gap-8",children:[e("div",{children:o(t,{toggled:a>0,children:[e(t.Header,{children:e(n,{on:a>0,className:"mr-2"})}),e(t.Body,{className:"flex gap-10",children:e(C,{trackWidth:6,buttonColor:"var(--echo-card)",min:0,max:20,value:a,size:60,onChange:m,topLabel:"Attack",bottomLabel:o("span",{className:"-mt-2 text-sm",children:[a," ms"]})})})]})}),e("div",{children:o(t,{toggled:d,className:"block data-[toggled=true]:border-indigo-500",children:[e(t.Header,{children:e(b,{checked:d,onClick:g,color:"#6366f1",children:"Delay"})}),o(t.Body,{className:"text-muted-foreground flex flex-col gap-2",children:[e("div",{children:"Click radio to toggle this card"}),e("div",{children:e(f,{disabled:!d,toggled:c,radius:"sm",className:"data-[toggled=true]:bg-indigo-500",onClick:()=>p(!c),children:"Mono Bass"})}),e(x,{disabled:!d,className:"my-3",classNames:{progress:"bg-indigo-500 group-data-[disabled=true]:bg-muted"}})]})]})})]})},A=()=>e(l,{code:`<Card>
+  <Card.Header> Header </Card.Header>
+  <Card.Body> 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum natus dolorem sit quas placeat cupiditate hic voluptatem blanditiis minima magnam asperiores laudantium deserunt tenetur eveniet soluta fuga, reprehenderit beatae repellendus. 
+  </Card.Body>
+</Card>`,scope:{Card:t}}),B=()=>e(l,{code:`<Card toggled>
+  <Card.Header> 
+    <Light on className='mr-3' /> Header 
+  </Card.Header>
+  <Card.Body> 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum natus dolorem sit quas placeat cupiditate hic voluptatem blanditiis minima magnam asperiores laudantium deserunt tenetur eveniet soluta fuga, reprehenderit beatae repellendus. 
+  </Card.Body>
+</Card>`,scope:{Card:t,Light:n}}),H=()=>e(l,{code:"<CardActualScenario />",scope:{CardActualScenario:v},type:"link",url:"https://github.com/codeacme17/echo-ui/blob/main/docs/src/components/Example/CardActualScenario.tsx"}),S=()=>e(y,{data:[{attribute:"toggled",description:"Is the card activated",type:e(i,{children:"boolean"}),default:e(i,{children:"false"})}]});export{H as A,S as C,A as D,B as T};
