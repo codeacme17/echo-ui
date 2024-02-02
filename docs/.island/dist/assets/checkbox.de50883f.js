407a8646288c288240675ea63b32f93c810f7c2a
codeacme17 <vir_kings@qq.com>
2024-02-02 17:37:57 +0800

    chore(docs): build dist

---
+++ docs/.island/dist/assets/checkbox.de50883f.js
@@ -0,0 +1,145 @@
+import{jsx as n,jsxs as t,Fragment as o}from"react/jsx-runtime";import{C as c}from"./CodeBlock.de027be4.js";import{D as h,a as i,S as d,C as s,G as l,b,c as x}from"./Checkbox.9455de79.js";import"react";import"./index.2d091e0a.js";import"./index.6a66cf71.js";import"../client-entry.js";import"./chunk-FXLYF44B.3b11858b.js";import"react-dom";const T=void 0,P=[{id:"\u5F15\u5165",text:"\u5F15\u5165",depth:2},{id:"\u4EE3\u7801\u6F14\u793A",text:"\u4EE3\u7801\u6F14\u793A",depth:2},{id:"\u7981\u7528\u72B6\u6001",text:"\u7981\u7528\u72B6\u6001",depth:3},{id:"\u5C3A\u5BF8",text:"\u5C3A\u5BF8",depth:3},{id:"\u989C\u8272",text:"\u989C\u8272",depth:3},{id:"\u591A\u9009\u7EC4",text:"\u591A\u9009\u7EC4",depth:3},{id:"\u6807\u7B7E-data",text:"\u6807\u7B7E Data",depth:2},{id:"api",text:"API",depth:2},{id:"checkbox",text:"Checkbox",depth:3},{id:"checkboxgroup",text:"Checkbox.Group",depth:3},{id:"\u7C7B\u578B\u58F0\u660E",text:"\u7C7B\u578B\u58F0\u660E",depth:2}],H="Checkbox \u591A\u9009\u6846";function r(a){const e=Object.assign({h1:"h1",a:"a",h2:"h2",h3:"h3",ul:"ul",li:"li",p:"p",code:"code",div:"div"},a.components);return t(o,{children:[t(e.h1,{id:"checkbox-\u591A\u9009\u6846",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#checkbox-\u591A\u9009\u6846",children:"#"}),"Checkbox \u591A\u9009\u6846"]}),`
+`,t(e.h2,{id:"\u5F15\u5165",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u5F15\u5165",children:"#"}),"\u5F15\u5165"]}),`
+`,n(c,{code:"import { Checkbox } from 'echo-ui'"}),`
+`,t(e.h2,{id:"\u4EE3\u7801\u6F14\u793A",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u4EE3\u7801\u6F14\u793A",children:"#"}),"\u4EE3\u7801\u6F14\u793A"]}),`
+`,n(h,{}),`
+`,t(e.h3,{id:"\u7981\u7528\u72B6\u6001",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u7981\u7528\u72B6\u6001",children:"#"}),"\u7981\u7528\u72B6\u6001"]}),`
+`,n(i,{}),`
+`,t(e.h3,{id:"\u5C3A\u5BF8",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u5C3A\u5BF8",children:"#"}),"\u5C3A\u5BF8"]}),`
+`,n(d,{}),`
+`,t(e.h3,{id:"\u989C\u8272",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u989C\u8272",children:"#"}),"\u989C\u8272"]}),`
+`,n(s,{}),`
+`,t(e.h3,{id:"\u591A\u9009\u7EC4",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u591A\u9009\u7EC4",children:"#"}),"\u591A\u9009\u7EC4"]}),`
+`,n(l,{}),`
+`,t(e.h2,{id:"\u6807\u7B7E-data",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u6807\u7B7E-data",children:"#"}),"\u6807\u7B7E Data"]}),`
+`,t(e.ul,{children:[`
+`,t(e.li,{children:[`
+`,t(e.p,{children:[n(e.code,{children:"data-checked"}),": \u5F53\u524D\u591A\u9009\u6846\u662F\u5426\u5904\u4E8E\u6FC0\u6D3B\u72B6\u6001"]}),`
+`]}),`
+`,t(e.li,{children:[`
+`,t(e.p,{children:[n(e.code,{children:"data-disabled"}),": \u5F53\u524D\u591A\u9009\u6846\u662F\u5426\u5904\u4E8E\u7981\u7528\u72B6\u6001"]}),`
+`]}),`
+`]}),`
+`,t(e.h2,{id:"api",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#api",children:"#"}),"API"]}),`
+`,t(e.h3,{id:"checkbox",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#checkbox",children:"#"}),"Checkbox"]}),`
+`,t(e.div,{className:"island-directive tip",children:[n(e.p,{className:"island-directive-title",children:"TIP"}),n(e.div,{className:"island-directive-content",children:t(e.p,{children:["\u652F\u6301\u539F\u751F\u7C7B\u578B\u4E3A ",n(e.code,{children:"checkbox"})," \u7684 ",n(e.code,{children:"input"})," \u6807\u7B7E\u7684\u5176\u4ED6\u6240\u6709\u5C5E\u6027"]})})]}),`
+`,n(b,{}),`
+`,t(e.h3,{id:"checkboxgroup",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#checkboxgroup",children:"#"}),"Checkbox.Group"]}),`
+`,n(x,{}),`
+`,t(e.h2,{id:"\u7C7B\u578B\u58F0\u660E",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#\u7C7B\u578B\u58F0\u660E",children:"#"}),"\u7C7B\u578B\u58F0\u660E"]}),`
+`,n(c,{code:`interface AbstractCheckboxProps<T> extends Omit<React.HTMLAttributes<T>, 'onChange' | 'value'> {
+  value?: any
+  disabled?: boolean
+  checked?: boolean
+  size?: 'sm' | 'md' | 'lg'
+  color?: string
+  classNames?: { label?: string }
+  styles?: { label?: React.CSSProperties }
+  onChange?: (e: CheckboxChangeEvent) => void
+}
+
+export interface CheckboxProps extends AbstractCheckboxProps<HTMLInputElement> {
+  onClick?: React.MouseEventHandler<HTMLElement>
+  onMouseEnter?: React.MouseEventHandler<HTMLElement>
+  onMouseLeave?: React.MouseEventHandler<HTMLElement>
+}
+
+export interface CheckboxGroupProps extends AbstractCheckboxProps<HTMLDivElement> {
+  value?: any[]
+  classNames?: { checkbox?: string } & AbstractCheckboxProps<HTMLDivElement>['classNames']
+  styles?: { checkbox?: React.CSSProperties } & AbstractCheckboxProps<HTMLDivElement>['styles']
+}
+
+export interface CheckboxChangeEvent {
+  value: any
+  nativeEvent: React.ChangeEvent<HTMLInputElement>
+}
+
+export interface CheckboxRef extends HTMLLabelElement {}
+
+export interface CheckboxGroupRef extends HTMLDivElement {}`})]})}function L(a={}){const{wrapper:e}=a.components||{};return e?n(e,Object.assign({},a,{children:n(r,a)})):r(a)}const g="2024/1/10 10:10:15",A=`import { CodeBlock } from '../../src/components/CodeBlock.tsx'
+import { Default, Disabled, Size, Color, Group } from '../../src/components/UsageBox/checkbox.tsx'
+import { CheckboxAPITable, CheckboxGroupAPITable } from '../../src/components/APITable/Checkbox.tsx'
+
+# Checkbox \u591A\u9009\u6846
+
+## \u5F15\u5165
+
+<CodeBlock code={\`import { Checkbox } from 'echo-ui'\`} />
+
+## \u4EE3\u7801\u6F14\u793A
+
+<Default />
+
+### \u7981\u7528\u72B6\u6001
+
+<Disabled />
+
+### \u5C3A\u5BF8
+
+<Size />
+
+### \u989C\u8272
+
+<Color />
+
+### \u591A\u9009\u7EC4
+
+<Group />
+
+## \u6807\u7B7E Data
+
+- \`data-checked\`: \u5F53\u524D\u591A\u9009\u6846\u662F\u5426\u5904\u4E8E\u6FC0\u6D3B\u72B6\u6001
+
+- \`data-disabled\`: \u5F53\u524D\u591A\u9009\u6846\u662F\u5426\u5904\u4E8E\u7981\u7528\u72B6\u6001
+
+## API
+
+### Checkbox
+
+:::tip
+
+\u652F\u6301\u539F\u751F\u7C7B\u578B\u4E3A \`checkbox\` \u7684 \`input\` \u6807\u7B7E\u7684\u5176\u4ED6\u6240\u6709\u5C5E\u6027
+
+:::
+
+<CheckboxAPITable />
+
+### Checkbox.Group
+
+<CheckboxGroupAPITable />
+
+## \u7C7B\u578B\u58F0\u660E
+
+<CodeBlock code={\`interface AbstractCheckboxProps<T> extends Omit<React.HTMLAttributes<T>, 'onChange' | 'value'> {
+  value?: any
+  disabled?: boolean
+  checked?: boolean
+  size?: 'sm' | 'md' | 'lg'
+  color?: string
+  classNames?: { label?: string }
+  styles?: { label?: React.CSSProperties }
+  onChange?: (e: CheckboxChangeEvent) => void
+}
+
+export interface CheckboxProps extends AbstractCheckboxProps<HTMLInputElement> {
+  onClick?: React.MouseEventHandler<HTMLElement>
+  onMouseEnter?: React.MouseEventHandler<HTMLElement>
+  onMouseLeave?: React.MouseEventHandler<HTMLElement>
+}
+
+export interface CheckboxGroupProps extends AbstractCheckboxProps<HTMLDivElement> {
+  value?: any[]
+  classNames?: { checkbox?: string } & AbstractCheckboxProps<HTMLDivElement>['classNames']
+  styles?: { checkbox?: React.CSSProperties } & AbstractCheckboxProps<HTMLDivElement>['styles']
+}
+
+export interface CheckboxChangeEvent {
+  value: any
+  nativeEvent: React.ChangeEvent<HTMLInputElement>
+}
+
+export interface CheckboxRef extends HTMLLabelElement {}
+
+export interface CheckboxGroupRef extends HTMLDivElement {}\`} />
+`;export{A as content,L as default,T as frontmatter,g as lastUpdatedTime,H as title,P as toc};
