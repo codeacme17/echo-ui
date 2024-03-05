c1fe2624eefa639ffb729014a12947ff294855e5
leyoonafr <vir_kings@qq.com>
2024-03-05 13:49:18 +0800

    chore(LFO): build dist

---
+++ docs/.island/dist/assets/Button.ed4115fb.js
@@ -0,0 +1,40 @@
+import{U as n,p as s,x as d,C as i,T as l,S as r,A as u,c as e}from"./index.83878d6a.js";import{jsx as t,jsxs as c,Fragment as p}from"react/jsx-runtime";const h=()=>t(n,{code:"<Button> Button </Button>",scope:{Button:s}}),b=()=>t(n,{code:"<Button toggled> Toggled </Button>",scope:{Button:s}}),m=()=>t(n,{code:"<Button disabled> Disabled </Button>",scope:{Button:s}}),f=()=>t(n,{code:`<div className="flex gap-4 items-center">
+  <Button size="sm">
+    Small
+  </Button>  
+  <Button size="md">
+    Medium
+  </Button>  
+  <Button size="lg">
+    Large
+  </Button>  
+</div>`,scope:{Button:s}}),y=()=>t(n,{code:`<div className="flex gap-4 items-center">
+  <Button radius="none">
+    None
+  </Button>  
+  <Button radius="sm">
+    Small
+  </Button>  
+  <Button radius="md">
+    Medium
+  </Button>  
+  <Button radius="lg">
+    Large
+  </Button>
+  <Button radius="full">
+    Full
+  </Button>  
+</div>`,scope:{Button:s}}),v=()=>t(n,{code:`<Button.Group>
+  <Button value={1} className="data-[toggled=true]:bg-red-500">
+    Sine
+  </Button>
+  <Button value={2}>
+    Square
+  </Button>
+  <Button value={3}>
+    Sawtooth
+  </Button>
+  <Button value={4}>
+    Triangle
+  </Button>
+</Button.Group>`,scope:{Button:s,SineIcon:d,SawtoothIcon:i,SquareIcon:l,TriangleIcon:r}}),T=()=>t(u,{data:[{attribute:"value",description:c(p,{children:["The bound value (only effective in ",t(e,{children:"`Group`"}),")."]}),type:t(e,{children:"any"}),default:"-"},{attribute:"disabled",description:"Indicates whether the button is disabled",type:t(e,{children:"boolean"}),default:t(e,{children:"false"})},{attribute:"size",description:"Button size",type:t(e,{children:"'sm' | 'md' | 'lg'"}),default:t(e,{children:"'md'"})},{attribute:"radius",description:"Button border radius",type:t(e,{children:"'none' | 'sm' | 'md' | 'lg' | 'full'"}),default:t(e,{children:"'md'"})},{attribute:"toggled",description:"Indicates whether the button is toggled",type:t(e,{children:"boolean"}),default:t(e,{children:"false"})},{attribute:"onToggleChange",description:"Callback function when the toggle state changes",type:t(e,{children:"(toggled: boolean) => void"}),default:"-"}]}),S=()=>t(u,{data:[{attribute:"value",description:"The value associated with the button group. If the value is an array, the button group will be treated as a multi-select group. Otherwise, it will be treated as a single-select group.",type:t(e,{children:"any"}),default:"-"},{attribute:"disabled",description:"Indicates whether buttons in the button group are disabled",type:t(e,{children:"boolean"}),default:t(e,{children:"false"})},{attribute:"size",description:"Button size",type:t(e,{children:"'sm' | 'md' | 'lg'"}),default:t(e,{children:"'md'"})},{attribute:"radius",description:"Button border radius",type:t(e,{children:"'none' | 'sm' | 'md' | 'lg' | 'full'"}),default:t(e,{children:"'md'"})},{attribute:"classNames",description:"Allows setting custom class names for buttons and toggle states",type:t(e,{children:"{ button?: string }"}),default:"-"},{attribute:"styles",description:"Allows setting custom styles for buttons and toggle states",type:t(e,{children:"{ button?: React.CSSProperties }"}),default:"-"},{attribute:"onChange",description:"Callback function when options change",type:t(e,{children:"(values: any) => void"}),default:"-"}]});export{T as B,h as D,v as G,y as R,f as S,b as T,m as a,S as b};
---
+++ docs/.island/dist/assets/play.04387a46.js
@@ -0,0 +1,6 @@
+import{c as o}from"./CodeBlock.de027be4.js";/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=o("Play",[["polygon",{points:"5 3 19 12 5 21 5 3",key:"191637"}]]);export{c as P};
