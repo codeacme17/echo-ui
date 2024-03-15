d988b3a0a9107cdea3c6913f62634335f6c4881a
leyoonafr <vir_kings@qq.com>
2024-03-15 13:48:04 +0800

    chore(docs): build dist

--- docs/.island/dist/assets/Light.a5aca436.js
+++
@@ -1,4 +0,0 @@
-import{U as c,A as s,a as o}from"./index.036b4c00.js";import{jsx as t}from"react/jsx-runtime";import{L as i}from"./index.efdc3921.js";const h=()=>t(c,{code:"<Light />",scope:{Light:i}}),l=()=>t(c,{code:"<Light on />",scope:{Light:i}}),p=()=>t(c,{code:`<div className="flex gap-6">
-  <Light color='#6366f1'/>
-  <Light on color='#6366f1'/>
-</div>`,scope:{Light:i}}),g=()=>t(c,{code:"<Light on size={17} />",scope:{Light:i}}),f=()=>t(s,{data:[{attribute:"on",description:"Whether the indicator light is on",type:t(o,{children:"boolean"}),default:t(o,{children:"false"})},{attribute:"size",description:"Size of the indicator light",type:t(o,{children:"number | string"}),default:t(o,{children:"0.75rem"})},{attribute:"color",description:"Color of the indicator light",type:t(o,{children:"string"}),default:t(o,{children:"var(--echo-primary)"})}]});export{p as C,h as D,f as L,l as O,g as S};
